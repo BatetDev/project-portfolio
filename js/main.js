43f94fb8e5afd7dbc8452c11c910a37865770a30
fr4nbtt <batet.francisco@gmail.com>
2025-01-18 20:38:20 -0300

    feat: add mobile menu toggle functionality

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
 "use strict";
+
+function toggleMenu() {
+  const menu = document.querySelector(".mobile-nav__links");
+  const icon = document.querySelector(".mobile-nav__icon");
+
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
