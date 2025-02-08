8e5104b8de75b7c51123cc8ae1370411cc28efc4
Fran Batet <batet.francisco@gmail.com>
2025-02-08 20:49:19 -0300

    style: Add Orbitron and Roboto Mono to Tailwind font stack

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,8 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Poppins', 'sans-serif'],
+        orbitron: ['Orbitron', 'sans-serif'],
+        robotoMono: ['Roboto Mono', 'monospace'],
       },
     },
   },
